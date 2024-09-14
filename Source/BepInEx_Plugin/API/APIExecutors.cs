a5131ea221d73d6bcd047b4f23e8ab5fcf9a03ac
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2024-09-14 13:02:15 +0200

    Commit, cause I have to check something on working branch

--- Source/BepInEx_Plugin/API/APIExecutors.cs
+++ Source/BepInEx_Plugin/API/APIExecutors.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace SuisApiExtension.API
 {
+	public static Dictionary<string, APIExecutors>
+
 	public class APIExecutorsExtended : MonoBehaviour
 	{
 		//public Executor_ExtendedDropImageRequest ExecutorInstance_ExtendedDropImageRequest;
